4288ddd81fd182916ebc27274bda3fa385bfd21c
BanTai01 <zahidul3864@.com>
2025-03-07 23:57:01 +0600

    Membership Operator

---
+++ membership_operators.py
@@ -0,0 +1,8 @@
+# in, not in
+
+print("sdididididid"in "didi")
+print("didididi"in "sisisisdididididididid")
+
+fruits = ['orange', 'apple', 'banana']
+print('orange'in fruits)
+print('cherry'in fruits)
