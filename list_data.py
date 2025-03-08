73b3d182b80a8e64aa7467e24e6ba6f2cea3520f
BanTai01 <zahidul3864@.com>
2025-03-08 23:20:13 +0600

    Add list_data

---
+++ list_data.py
@@ -0,0 +1,11 @@
+
+my_lst1 = ["ramesh", "suresh", "gonesh", "zahid"]
+my_lst2 = ["mesh", ]
+print(my_lst1[3])
+my_lst1.append("Tisha")
+print(my_lst1)
+my_lst1.insert(1, "sweet")
+print(my_lst1)
+
+new_group = my_lst1 + my_lst2
+print(new_group)
